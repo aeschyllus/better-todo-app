93550c43539e0878873fcb53de2d836cbad32f4c
aeschyllus <markanthony.lapada@tup.edu.ph>
2021-01-11 20:52:03 +0800

    Added "new list item" function

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,35 @@
+import React, {useState, useEffect} from 'react'
+import './assets/css/styles.css'
+import CreateListForm from './CreateListForm'
+
 function App() {
+  const [categories, setCategories] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:8000/api/category-list/')
+    .then(response => response.json())
+    .then(result => {
+      setCategories(result)
+    })
+  }, [categories])
+
+  let categoryList = categories.map(category => <li key={category.id} className="category">{category.name}</li>)
+
   return (
-    <div>
-      <p>Hello world</p>
-    </div>
+    <React.Fragment>
+      <h1 className="title">Stuff I need to do</h1>
+
+      {/* lists */}
+      <div className="categories">
+        <h2 className="categories-title">My lists</h2>
+        <ul className="categories-list">
+          {categoryList}
+        </ul>
+
+        {/* List form */}
+        <CreateListForm />
+      </div>
+    </React.Fragment>
   );
 }
 
